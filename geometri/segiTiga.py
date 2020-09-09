13a3865b9578596f7548992f7efcb0b6e6919617
aris winandi <winandiaris@gmail.com>
2020-09-09 13:29:42 +0700

    oop

--- geometri/segiTiga.py
+++ geometri/segiTiga.py
@@ -1,10 +1,15 @@
-class SegiTiga():
+from geometri.bangunRuang import BangunRuang
+
+
+class SegiTiga(BangunRuang):
     def __init__(self,a,t):
         self.a = a
         self.t = t
+    def test(self):
+        return 'test segitiga'
 
     def info(self):
-        return f'Luas segitiga dengan alas {self.a} dan {self.t} adalah'
+        return f'Menghitung rumus segitiga dengan alas {self.a} dan {self.t} adalah'
 
     def hitung_luas(self):
         return 0.5 * self.a * self.t
