5fd4da86252e8a71f42de061aa01a751d4dcdec1
aris winandi <winandiaris@gmail.com>
2020-09-09 13:29:13 +0700

    oop

--- geometri/persegiPanjang.py
+++ geometri/persegiPanjang.py
@@ -1,8 +1,13 @@
-class PersegiPanjang():
+from geometri.bangunRuang import BangunRuang
+
+
+class PersegiPanjang(BangunRuang):
     def __init__(self, p, l): # method/fungsi yang mendefinisikan argumen class, (self-->merujuk ke objek)
         #fungsi yang pertama kali dipanggil saat objek dibuat
         self.p = p
         self.l = l
+    def test(self):
+        return 'test persegi panjang'
 
     def info(self):
         return f'Menghitung rumus persegi panjang dengan panjang {self.p} dan lebar {self.l}'
